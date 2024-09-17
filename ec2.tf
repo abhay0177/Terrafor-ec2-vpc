0d9fbae56ff932f45a868f2991b31166038e042f
Abhay Solanki <solankiabhay916@gmail.com>
2024-09-18 02:12:37 +0530

    added user data

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
 #ec2-instance
 resource "aws_instance" "my_ec2_instance" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  subnet_id     = aws_subnet.my_subnet.id
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.my_subnet.id
   vpc_security_group_ids      = [aws_security_group.my_security_group.id]
   associate_public_ip_address = true
   key_name                    = aws_key_pair.deployer.key_name #key_name=resource attribute reference
@@ -10,4 +10,20 @@ resource "aws_instance" "my_ec2_instance" {
   tags = {
     Name = "MyEc2Instance"
   }
+  user_data = <<-EOF
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get install -y nginx
+sudo systemctl start nginx
+sudo systemctl enable nginx
+
+sudo apt-get -y update
+sudo apt-get install -y docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo chown $USER /var/run/docker.sock
+
+sudo apt-get install -y git 
+
+EOF
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,5 +1,5 @@
 ami_id        = "enter-ami-id-here"
 instance_type = "t2.micro"
-ports         = [22, 80, 443]
+ports         = [22, 80, 443, 3000]
 accessKey     = "enter-access-key-here"
 secretKey     = "enter-secret-key-here"
